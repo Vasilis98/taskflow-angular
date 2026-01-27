b08e3173222880a449ef092bd94916c364f501e6
Vasilis <billnick@hotmail.gr>
2026-01-27 19:40:06 +0200

    feat: created TaskCard component with Inputs

---
+++ src/app/task-card/task-card.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {NgClass} from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-task-card',
+  imports: [
+    NgClass
+  ],
+  templateUrl: './task-card.html',
+  styleUrl: './task-card.css',
+})
+export class TaskCard {
+
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() date: string = '';
+  @Input() priority: string = '';
+
+}
